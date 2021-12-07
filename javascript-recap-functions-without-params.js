d9f7d04b638b3a107aa2c51dcfcce58fd13bd324
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-12-07 10:22:03 +0100

    enza

---
+++ javascript-recap-functions-without-params.js
@@ -0,0 +1,13 @@
+const a = 7;
+const b = 8; 
+const c = 9;
+const d = 10; 
+
+function inVoice() {
+    let ergebnis = (a + b) * c / d;
+    console.log("(a + b) * c / d = ", ergebnis); // 23
+}
+
+
+inVoice();
+

90b009c6f4b2112a751d043b067862823f463d59
sprotte <metatron1972@live.de>
2021-12-07 10:22:25 +0100

    sprotte

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const ersteZahl = 7;
 const zweiteZahl = 8;
 const driteZahl = 9;
 const vierteZahl = 10;
+
+
+function ergebniss() {
+    let resultat = ersteZahl + zweiteZahl * driteZahl / vierteZahl;
+    console.log("addition",resultat);
+  }
+  
+  
+  
+  ergebniss();
+ 
+  

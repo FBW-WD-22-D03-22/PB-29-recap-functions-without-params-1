4911e25d4dadeb168ae3c1428ddb65a6fbbce4db
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-12-07 11:48:43 +0100

    js

---
+++ index.js
@@ -0,0 +1,10 @@
+const ersteZahl = 7;
+const  zweiteZahl = 8;
+const dritteZahl = 9;
+const vierteZahl = 10;
+
+function addition(){
+    return (ersteZahl + zweiteZahl)*dritteZahl/vierteZahl;
+}
+let ergibns = addition();
+console.log(ergibns);

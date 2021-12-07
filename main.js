a1a280fd3b8b9fe777d94e9353246941a0c79c3d
kawanaza <kawanbabani@gmail.com>
2021-12-07 11:34:53 +0100

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+const ersteNummer = 7;
+const zweiteNummer = 8;
+const drieteNummer = 9;
+const vierteNummer = 10;
+
+function myfunction(a,b,c,d){
+  let summe = (a + b)* c / d;
+  console.log("ersteNummer+zweiteNummer * drieteNummer / vierteNummer",summe);
+}
+
+
+myfunction(7,8,9,10);

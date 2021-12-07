04818c1f41257089e6da9dfce760d12ba8199c1a
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-12-07 11:28:02 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,15 @@
+const number = 7;
+const number2 = 8;
+const number3 = 9;
+const number4 = 10;
+
+function rechnen(){
+    let ergebnis = (number + number2) * number3 / number4;
+    console.log(ergebnis);
+}
+
+let ergebnis = rechnen();
+
+
+
+rechnen();

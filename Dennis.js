7ee2718b00ceca006b1ec7a2de62580cc504f87d
Dennisbullys <d.rochus@hotmail.de>
2021-12-07 10:27:23 +0100

    Dennis

---
+++ Dennis.js
@@ -0,0 +1,20 @@
+// # javascript-recap-functions-without-params
+
+// Erstelle eine Konstante mit der Zahl 7.
+
+
+const a = 7;
+const b = 8;
+const c = 9;
+const d =10;
+
+function rechnen(){
+    let ergebnis = (a + b) * c / d;
+    console.log(ergebnis);
+}
+
+let ergebnis = rechnen();
+
+
+
+rechnen();
